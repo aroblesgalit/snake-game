f289e5682a78fc6915103c41d653ae333ef43fe2
Alvin Galit <aroblesgalit@gmail.com>
2020-12-18 21:34:09 -0600

    Created a js file for snake logic

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
+
 let lastRenderTime = 0;
-const SNAKE_SPEED = 2;
 
 function main(currentTime) {
     window.requestAnimationFrame(main);
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
-    
-    console.log('Render');
+
     lastRenderTime = currentTime;
 
     update();
@@ -14,3 +14,11 @@ function main(currentTime) {
 }
 
 window.requestAnimationFrame(main);
+
+function update() {
+    updateSnake();
+}
+
+function draw() {
+    drawSnake();
+}
