562d823199a4f9ffc189f88881eaf012ff10ea88
Alvin Galit <aroblesgalit@gmail.com>
2020-12-18 21:40:35 -0600

    Created snakeBody array with initial obj that includes an x and y property, took snakeBody into the draw function to render it on the game-board

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js';
+const gameBoard = document.querySelector('#game-board');
 
 let lastRenderTime = 0;
 
@@ -20,5 +21,5 @@ function update() {
 }
 
 function draw() {
-    drawSnake();
+    drawSnake(gameBoard);
 }
