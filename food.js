bcf995a2e548721b17b24bbee868bd2ae4d22f55
Alvin Galit <aroblesgalit@gmail.com>
2020-12-20 11:44:06 -0800

    Created function that ads a segment to the snakebody when it eats the food

--- food.js
+++ food.js
@@ -1,10 +1,12 @@
+import { onSnake, expandSnake } from './snake.js';
+
 let food = { x: 1, y: 1 };
 const EXPANSION_RATE = 1;
 
 export function update() {
     if (onSnake(food)) {
         expandSnake(EXPANSION_RATE);
-        food = { x: 20, y: 10};
+        food = { x: 20, y: 10 };
     }
 }
 
