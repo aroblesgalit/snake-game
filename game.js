5f9b0e1233edd2ac63f82e7d26940fd0c22cd62b
Alvin Galit <aroblesgalit@gmail.com>
2020-12-24 14:32:49 -0800

    Fixed issue with score's not saving correctly in localStorage

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js';
 import { update as updateFood, draw as drawFood } from './food.js';
 import { outsideGrid } from './grid.js';
-import { update as updateScore, draw as drawScore } from './score.js';
+import { update as updateScore, draw as drawScore, addToHighscore, score } from './score.js';
 
 let lastRenderTime = 0;
 let gameOver = false;
@@ -10,6 +10,7 @@ const scoreBoard = document.querySelector('.score');
 
 function main(currentTime) {
     if (gameOver) {
+        addToHighscore(score);
         if (confirm('You lost. Press ok to restart.')) {
             window.location = '/';
         }
--- score.js
+++ score.js
@@ -1,7 +1,9 @@
 import { snakeBody } from './snake.js';
 
 export let score = 0;
-let highscores = getHighscores() || [];
+let highscores = [];
+// localStorage.setItem('highscores', JSON.stringify(highscores));
+highscores = getHighscores();
 
 export function update() {
     if (snakeBody.length > 1) {
@@ -15,27 +17,36 @@ export function draw(scoreBoard) {
 
 // Get highscores list from localStorage or []
 function getHighscores() {
-    return localStorage.getItem('highscores');
+    let storedHighscores = localStorage.getItem('highscores');
+    console.log('Running getHighscores...', storedHighscores);
+    storedHighscores = storedHighscores ? JSON.parse(storedHighscores) : [];
+    console.log('Grabbing highscores from localStorage: ', storedHighscores);
+    return storedHighscores;
 }
 
 export function addToHighscore(score) {
+    console.log('Checking highscore...')
     if (highscores.length === 3) {
-        const isNewHighscore = highscores.some(highscore => highscore < score); // Checks if any of the highscore is less than the current score
+        console.log('Checking if highscore board has 3 items...')
+        const isNewHighscore = highscores.some(record => record.highscore < score); // Checks if any of the highscore is less than the current score
         if (isNewHighscore) {
+            console.log('Score is a new highscore!')
             const newHighscoreName = prompt('Enter your name: ');
             highscores.push({ name: newHighscoreName, highscore: score });
-            highscores.sort((a, b) => a.highscore > b.highscore);
-            highscore.pop();
-            localStorage.setItem('highscores', highscores);
+            highscores.sort((a, b) => b.highscore - a.highscore);
+            highscores.pop();
+            localStorage.setItem('highscores', JSON.stringify(highscores));
+            console.log('Highscores stored in localStorage: ', highscores)
             return;
         } else {
+            console.log('Score did not make the highscore board');
             return;
         }
     } else if (highscores.length < 3) {
         const newHighscoreName = prompt('Enter your name: ');
         highscores.push({ name: newHighscoreName, highscore: score });
         highscores.sort((a, b) => a.highscore > b.highscore);
-        localStorage.setItem('highscores', highscores);
+        localStorage.setItem('highscores', JSON.stringify(highscores));
         return;
     }
 }
